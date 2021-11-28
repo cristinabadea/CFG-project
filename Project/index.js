134d64a544e005272abc1717aea9cf10fa3a0e35
Tina Georgescu <77801352+cristinabadea@users.noreply.github.com>
2021-11-28 11:41:58 +0000

    Moving to mac
    - saving files on mac

--- Project/index.js
+++ Project/index.js
@@ -1,7 +1,1 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var colorMenu = document.getElementById('nav-item');
-
-//     colorMenu.addEventListener("mouseenter", function () {
-//         colorMenu.style.color = "red";
-//     });
-// }
+/** @format */
