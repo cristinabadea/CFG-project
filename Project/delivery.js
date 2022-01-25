c14fed39fb75483b9433d94d91152dbd1b988b5d
Tina Georgescu <77801352+cristinabadea@users.noreply.github.com>
2022-01-25 02:46:04 +0000

    delivery page

---
+++ Project/delivery.js
@@ -0,0 +1,34 @@
+/** @format */
+
+var products = [
+  {
+    name: "Classic Dog",
+    id: 1,
+    category: "Hot Dogs",
+    brand: "American Goodies",
+    price: 4,
+  },
+  {
+    name: "Hamburger",
+    id: 2,
+    category: "Burgers",
+    brand: "Five Guys",
+    price: 6,
+  },
+  {
+    name: "BLT",
+    id: 3,
+    category: "Sandwiches",
+    brand: "American Goodies",
+    price: 3,
+  },
+  {
+    name: "Bacon Cheesburger",
+    id: 4,
+    category: "Burgers",
+    brand: "Five Guys",
+    price: 4,
+  },
+];
+
+document.getElementById("product-price").innerHTML = "£" + products[0].price;
